a42e5e189a9943d2a25b85374a67b9a9a3719dc9
tiberiu-telcean <tiberiu.telcean@gmail.com>
2020-08-30 14:23:20 +0300

    Add python startup file.
    Use `python3 -i startup.py` to start an interactive session after
    running the file.
    It includes matplotlib and num/scipy.

---
+++ scripts/startup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import matplotlib
+fig, ax = plt.subplots()
+mpl.axes.Axes.grid(ax, b=True)
